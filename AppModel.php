b83ed9299b1e5647ac62f1bb7cbabead21479f97
Maciek <maciej.jarmula@gmail.com>
2015-03-21 23:52:21 +0100

    model fix

--- AppModel.php
+++ AppModel.php
@@ -23,44 +23,46 @@ App::uses('Model', 'Model');
 class AppModel extends Model {
 
     public function saveAssociated($data = null, $options = array()) {
-        foreach ($data as $alias => $modelData) {
-            if (!empty($this->hasAndBelongsToMany[$alias])) {
 
+        foreach ($data as $alias => $modelData) {
+            
+            if (!empty($this->hasAndBelongsToMany[$alias])) {
                 $habtm = array();
                 $emId = false;
                 $Model = ClassRegistry::init($this->hasAndBelongsToMany[$alias]['className']);
 
-                if (isset($modelData[$options[$alias]['unique']])) {
-                    $searched = $modelData[$options[$alias]['unique']];
-                    $unique = ucfirst($options[$alias]['unique']);
-                    $fb = 'findBy' . $unique;
-                }
+                if (array_key_exists($alias,$options)) {
+                    if ( isset($modelData[$options[$alias]['unique']])) {
+                        $searched = $modelData[$options[$alias]['unique']];
+                        $unique = ucfirst($options[$alias]['unique']);
+                        $fb = 'findBy' . $unique;
+                    }
 
-                $em = $Model->$fb($searched);
-                if (!empty($em)) {
-                    $em = $em[$alias];
-                    $emId = $em['id'];
+                    $em = $Model->$fb($searched);
+                    if (!empty($em)) {
+                        $em = $em[$alias];
+                        $emId = $em['id'];
+                    }
                 }
-
+               
+                
                 foreach ($modelData as $modelDatum) {
-
                     if (empty($modelDatum['id'])) {
 
                         $Model->create();
                     }
-
-                    
                     if ($emId) {
                         $habtm[] = $emId;
+
                     } else {
                         $Model->save($modelData);
-                        $habtm[] = empty($modelDatum['id']) ? $Model->getInsertID() : $modelDatum['id'];
+                        $habtm[] = empty($modelData['id']) ? $Model->getInsertID() : $modelData['id'];
                     }
                 }
-
+               
                 $data[$alias] = array($alias => $habtm);
             }
-        }
+        } 
         return parent::saveAssociated($data, $options);
     }
 
